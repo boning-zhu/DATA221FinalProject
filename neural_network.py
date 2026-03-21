3ce80b66376e8d90e431447b43843a5419e8e33a
SitaraTrue <sitaratrue@gmail.com>
2026-03-20 23:10:02 -0600

    Compile neural network in neural_network.py

--- neural_network.py
+++ neural_network.py
@@ -19,3 +19,6 @@ hidden_layer = Dense(3)
 neural_network_model.add(hidden_layer)
 output_layer=Dense(1, activation='sigmoid')
 neural_network_model.add(output_layer)
+
+# Compile the mode
+neural_network_model.compile(loss='binary_crossentropy') # Using binary_crossentropy since this is a binary classification problem
