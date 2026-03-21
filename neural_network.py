235ce6b77546b581f5a4036370cc12cbc9d033ff
SitaraTrue <sitaratrue@gmail.com>
2026-03-20 23:00:09 -0600

    Set up neural network in neural_network.py

--- neural_network.py
+++ neural_network.py
@@ -1,3 +1,12 @@
-import pandas as pd
+import pandas as pd # for reading CSV
+import tensorflow as tf # for neural network
+from tensorflow.keras.models import Sequential # for neural network
 
+# Load the cleaned dataset into a data frame
 cleaned_diabetes_data = pd.read_csv("cleaned_diabetes.csv")
+
+# Set seed so model always returns the same results
+tf.random.set_seed(42)
+
+# Set up neural network
+neural_network_model = Sequential()
