70522623c00d2459f1033a0bc6cea107c2afe28c
SitaraTrue <sitaratrue@gmail.com>
2026-03-20 23:08:54 -0600

    Set up layers and neurons in neural_network.py

--- neural_network.py
+++ neural_network.py
@@ -1,6 +1,7 @@
-import pandas as pd # for reading CSV
-import tensorflow as tf # for neural network
-from tensorflow.keras.models import Sequential # for neural network
+import pandas as pd
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, InputLayer
 
 # Load the cleaned dataset into a data frame
 cleaned_diabetes_data = pd.read_csv("cleaned_diabetes.csv")
@@ -10,3 +11,11 @@ tf.random.set_seed(42)
 
 # Set up neural network
 neural_network_model = Sequential()
+
+# Define layers
+input_layer = InputLayer(input_shape=(8,)) # 8 input neurons for 8 features
+neural_network_model.add(input_layer)
+hidden_layer = Dense(3)
+neural_network_model.add(hidden_layer)
+output_layer=Dense(1, activation='sigmoid')
+neural_network_model.add(output_layer)
