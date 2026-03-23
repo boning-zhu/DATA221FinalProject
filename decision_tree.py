c557db54928294657147c63b6ef95cf522428b90
boning-zhu <boning.zhu@ucalgary.ca>
2026-03-23 20:31:24 +0000

    initialiize decision tree model

--- decision_tree.py
+++ decision_tree.py
@@ -18,3 +18,8 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
+
+#import decision tree model
+from sklearn.tree import DecisionTreeClassifier
+#initialize decision tree model
+model = DecisionTreeClassifier(random_state=42)
