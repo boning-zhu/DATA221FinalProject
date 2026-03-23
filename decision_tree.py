ad5e4d6fcc060bba13629510d342dab1592d821f
boning-zhu <boning.zhu@ucalgary.ca>
2026-03-23 20:25:02 +0000

    splite features and target

--- decision_tree.py
+++ decision_tree.py
@@ -4,5 +4,10 @@ import numpy as np
 data = pd.read_csv("data/cleaned_diabetes.csv")
 data.head()
 
+#explore dataset structure
 data.info()
 data.describe()
+
+#splite features and target
+X = data.drop("Outcome", axis=1)
+y = data["Outcome"]
