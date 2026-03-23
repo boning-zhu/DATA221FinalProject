7a96571a92fd53ff9674f87a426ab130f098e346
boning-zhu <boning.zhu@ucalgary.ca>
2026-03-23 20:28:52 +0000

    train test splite with random_state 42 and import decision tree model

--- decision_tree.py
+++ decision_tree.py
@@ -11,3 +11,10 @@ data.describe()
 #splite features and target
 X = data.drop("Outcome", axis=1)
 y = data["Outcome"]
+
+# train test split with random_state 42
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
